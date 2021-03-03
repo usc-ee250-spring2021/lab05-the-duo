5ad8a97b31946993baf907f7a05669d468a9c2c9
MaestroLiu3 <jliu8288@usc.edu>
2021-03-02 20:39:38 -0800

    draft

--- ee250/lab05/vm_subscriber.py
+++ ee250/lab05/vm_subscriber.py
@@ -7,13 +7,15 @@ import time
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
-
+    client.subscribe("jliu8288/ultrasonicRanger")
     #subscribe to the ultrasonic ranger topic here
-
+    client.subscribe("INSERT_RPI_HOSTNAME_HERE/customCallback")
+    client.message_callback_add("INSERT_RPI_HOSTNAME_HERE/customCallback", custom_callback)
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
-
+    str = "VM: [VALUE] cm"
+    print(str.decode('base64','strict'))
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
