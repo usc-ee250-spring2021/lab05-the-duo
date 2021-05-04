a96137556fb840c8a3710d14901b00e9969dee57
MaestroLiu3 <jliu8288@usc.edu>
2021-05-03 23:15:12 -0700

    tuner

---
+++ ee250/lab05/lcd_tuner2.py
@@ -0,0 +1,145 @@
+import paho.mqtt.client as mqtt
+import time
+#import PWM
+#import grovepi
+import grove_rgb_lcd
+from grove_rgb_lcd import *
+import statistics
+
+# GrovePi + Grove Buzzer
+import time
+import grovepi
+
+# Connect the Grove Buzzer to digital port D8
+# SIG,NC,VCC,GND
+buzzer = 3
+button = 4
+grovepi.pinMode(button,"INPUT")
+#note_produced = 69
+tones = {
+"B0": 31,
+"B1": 62,
+"C2": 65,
+"CS2": 69,
+"D2": 73,
+"DS2": 78,
+"E2": 82,
+"F2": 87,
+"FS2": 93,
+"G2": 98,
+"GS2": 104,
+"A2": 110,
+"AS2": 117,
+"B2": 123,
+"C3": 131,
+"CS3": 139,
+"D3": 147,
+"DS3": 156,
+"E3": 165,
+"F3": 175,
+"FS3": 185,
+"G3": 196,
+"GS3": 208,
+"A3": 220,
+"AS3": 233,
+"B3": 247,
+"C4": 262,
+"CS4": 277,
+"D4": 294,
+"DS4": 311,
+"E4": 330,
+"F4": 349,
+"FS4": 370,
+"G4": 392,
+"GS4": 415,
+"A4": 440,
+"AS4": 466,
+"B4": 494,
+"C5": 523,
+"CS5": 554,
+"D5": 587,
+"DS5": 622,
+"E5": 659,
+"F5": 698,
+"FS5": 740,
+"G5": 784,
+"GS5": 831,
+"A5": 880,
+"AS5": 932,
+"B5": 988,
+"C6": 1047,
+"CS6": 1109,
+"D6": 1175,
+"DS6": 1245,
+"E6": 1319,
+"F6": 1397,
+"FS6": 1480,
+"G6": 1568,
+"GS6": 1661,
+"A6": 1760,
+"AS6": 1865,
+"B6": 1976,
+"C7": 2093,
+"CS7": 2217,
+"D7": 2349,
+"DS7": 2489,
+"E7": 2637,
+"F7": 2794,
+"FS7": 2960,
+"G7": 3136,
+"GS7": 3322,
+"A7": 3520,
+"AS7": 3729,
+"B7": 3951,
+"C8": 4186,
+"CS8": 4435,
+"D8": 4699,
+"DS8": 4978
+}
+
+def main(file):
+    print("Importing {}".format(file))
+    audio = AudioSegment.from_mp3(file)
+
+    sample_count = audio.frame_count()
+    sample_rate = audio.frame_rate
+    samples = audio.get_array_of_samples()
+
+    print("Number of channels: " + str(audio.channels))
+    print("Sample count: " + str(sample_count))
+    print("Sample rate: " + str(sample_rate))
+    print("Sample width: " + str(audio.sample_width))
+
+    period = 1/sample_rate                     #the period of each sample
+    duration = sample_count/sample_rate         #length of full audio in seconds
+
+    slice_sample_size = int(SLICE_SIZE*sample_rate)   #get the number of elements expected for [SLICE_SIZE] seconds
+
+    n = slice_sample_size                            #n is the number of elements in the slice
+
+    #generating the frequency spectrum
+    k = np.arange(n)                                #k is an array from 0 to [n] with a step of 1
+    slice_duration = n/sample_rate                   #slice_duration is the length of time the sample slice is (seconds)
+    frq = k/slice_duration                          #generate the frequencies by dividing every element of k by slice_duration
+
+    #max_frq_idx = int(MAX_FRQ*slice_duration)       #get the index of the maximum frequency (2000)
+    frq = frq[range(5000)]                   #truncate the frequency array so it goes from 0 to 5000 Hz
+    note_produced = int(statistics.mean(frq))
+    print(note_produced)
+    #grovepi.pinMode(buzzer,"OUTPUT")
+    while True:
+        buttonpress = grovepi.digitalRead(button)
+        if(buttonpress):
+            print("buttonpressed")
+            for i in tones:
+                if(note_produced == tones[i]):
+                    grove_rgb_lcd.setText(i)
+                    break 
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2 or not os.path.isfile(sys.argv[1]):
+        print("Usage: decode.py [file]")
+        exit(1)
+    main(sys.argv[1])
+
+
